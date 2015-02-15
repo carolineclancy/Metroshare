5cb768fb0b1310a92d906a39fd283f0a0865f409
Caroline Clancy <carolineclancy97@gmail.com>
2015-02-15 06:46:20 -0500

    added login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,19 +8,34 @@ class ApplicationController < Sinatra::Base #inheriting all sorts of info
     set :views, 'app/views'
   end
 
+  post '/log_in' do
+    @new_post = Post.new(params[:user], params[:message], params[:time], params[:location])
+    @user = User.new(params[:name])
+    erb :log_in
+    redirect ('metrosharers')
+  end
+
+  get '/log_in' do
+    @names = User.show_name
+    erb :log_in
+  end
+
   get '/' do
     @messages = Post.show_all_messages
-    
+    @names = User.show_name
     erb :metro_share
   end
 
   post '/metrosharers' do
-    new_post = Post.new(params[:user], params[:message], params[:time])
+    new_post = Post.new(params[:user], params[:message], params[:time], params[:location])
+    @user = User.new(params[:name])
+    erb :metro_share
     redirect ('metrosharers')
   end
 
   get '/metrosharers' do
     @messages = Post.show_all_messages
+    @names = User.show_name
     erb :metro_share
   end
 end
--- app/models/metro_share.rb
+++ app/models/metro_share.rb
@@ -1,12 +1,31 @@
+class User
+  attr_accessor :name
+
+  NAME = []
+
+  def initialize(name)
+    @name = name
+    NAME << self
+  end
+
+
+  def self.show_name
+    NAME
+  end
+
+
+end
+
 class Post
-  attr_accessor :user, :message, :time
+  attr_accessor :user, :message, :time, :location
 
   ALL_MESSAGES = []
 
-  def initialize(user, message, time)
+  def initialize(user, message, time, location)
     @user = user
     @message = message
     @time = time
+    @location = location
     ALL_MESSAGES << self
   end
 
